63d594b76f546641a449322663f3dda3019a326f
Saugat <saugatghimire321@gmail.com>
2022-11-16 22:58:09 +0545

    Add JWT

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import UserController from '../controllers/userController.js'
-
+import validateToken from '../middleware/validateToken.js';
 
 const router = express.Router()
 const userController= new UserController();
 
 router.post('/signup',userController.addUser);
-router.get('/',userController.getAllUsers);
-router.get('/:id',userController.getSingleUser);
-router.put('/:id',userController.updateUser);
-router.delete('/:id',userController.deleteUser);
+router.post('/login',userController.loginUser);
+router.get('/',validateToken,userController.getAllUsers);
+router.get('/:id',validateToken,userController.getSingleUser);
+router.put('/:id',validateToken,userController.updateUser);
+router.delete('/:id',validateToken,userController.deleteUser);
 
 // router.post('/login',user.login);
 
