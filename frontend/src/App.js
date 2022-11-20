4edbedad63b915ec95f2e0edb2c3348c0feedb7d
susilnem <susilsaurav@gmail.com>
2022-11-20 21:56:48 +0545

    Add mapbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import Map, { Marker } from "react-map-gl";
+import { HiLocationMarker } from "react-icons/hi";
 function App() {
   return (
-    <div className=" text-white text-xl font-semibold container mx-auto text-center bg-red-500 rounded-xl shadow border p-8 m-10">
-      Hello world
-    </div>
+    <Map
+      initialViewState={{
+        longitude: 85.315716,
+        latitude: 27.6835509,
+        zoom: 13,
+      }}
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      <Marker
+        longitude={85.315716}
+        latitude={27.6835509}
+        anchor="bottom"
+        style={{ size: "3rem" }}
+      >
+        <HiLocationMarker />
+      </Marker>
+    </Map>
   );
 }
 
