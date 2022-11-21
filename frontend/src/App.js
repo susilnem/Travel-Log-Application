18c526d8a57890ba57362b808a196c3defa4a6d0
susilnem <susilsaurav@gmail.com>
2022-11-21 22:13:57 +0545

    add marker location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
-import Map, { Marker } from "react-map-gl";
-import { HiLocationMarker } from "react-icons/hi";
+import * as React from "react";
+import Map, { Marker, GeolocateControl, Popup } from "react-map-gl";
+// import { HiLocationMarker } from "react-icons/hi";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 function App() {
+  const [showPopup, setShowPopup] = React.useState(true);
+  const geolocateControlRef = React.useCallback((ref) => {
+    if (ref) {
+      // Activate as soon as the control is loaded
+      ref.trigger();
+    }
+  }, []);
   return (
     <Map
       initialViewState={{
         longitude: 85.315716,
         latitude: 27.6835509,
-        zoom: 13,
+        zoom: 20,
       }}
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-      <Marker
-        longitude={85.315716}
-        latitude={27.6835509}
-        anchor="bottom"
-        style={{ size: "3rem" }}
-      >
-        <HiLocationMarker />
-      </Marker>
+      <GeolocateControl
+        ref={geolocateControlRef}
+        positionOptions={{ enableHighAccuracy: true }}
+        trackUserLocation={true}
+      ></GeolocateControl>
+      {showPopup && (
+        <Popup
+          className="w-full p-3"
+          longitude={85.315716}
+          latitude={27.6835509}
+          anchor="bottom"
+          closeOnClick={false}
+          onClose={() => setShowPopup(false)}
+        >
+          <div className=" flex flex-col text-2xl">
+            <label>Title -</label>
+            <label>Description -</label>
+            <label>Rating -</label>
+            <label>Image</label>
+          </div>
+        </Popup>
+      )}
     </Map>
   );
 }
